40cf9adb542528bcf668765802d523534a35ad17
Samuel <samuelalves681@gmail>
2022-12-28 16:24:53 -0300

    signin service and controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -12,7 +12,7 @@ export class AuthController {
   }
 
   @Post('signin')
-  sigin() {
-    return 'signin';
+  sigin(@Body() dto: AuthDto) {
+    return this.authService.signin(dto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
@@ -10,21 +10,49 @@ export class AuthService {
   async signup(dto: AuthDto) {
     const { email, password } = dto;
     const hashedPassword = await argon.hash(password);
-    const user = await this.prismaService.user.create({
-      data: {
-        email,
-        hash: hashedPassword,
+
+    try {
+      const user = await this.prismaService.user.create({
+        data: {
+          email,
+          hash: hashedPassword,
+        },
+        // select: {
+        //   id: true,
+        //   email: true,
+        //   createdAt: true,
+        // },
+      });
+
+      delete user.hash;
+      return user;
+    } catch (error) {
+      if (error?.code === 'P2002') {
+        throw new ForbiddenException('Credentials taken');
+      }
+
+      throw error;
+    }
+  }
+
+  async signin(dto: AuthDto) {
+    const user = await this.prismaService.user.findUnique({
+      where: {
+        email: dto.email,
       },
-      // select: {
-      //   id: true,
-      //   email: true,
-      //   createdAt: true,
-      // },
     });
+
+    if (!user) {
+      throw new ForbiddenException('Invalid credentials');
+    }
+
+    const validPassword = await argon.verify(user.hash, dto.password);
+
+    if (!validPassword) {
+      throw new ForbiddenException('Invalid credentials');
+    }
 
     delete user.hash;
     return user;
   }
-
-  signin() {}
 }
